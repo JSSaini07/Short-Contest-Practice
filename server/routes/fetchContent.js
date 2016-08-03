80ecedc11f625ee712c3f1f11de1f5c4c8920f8e
JSSaini07 <mr.jssaini07@gmail.com>
2016-08-04 00:20:04 +0530

    scrape codechef problems

--- server/routes/fetchContent.js
+++ server/routes/fetchContent.js
@@ -37,26 +37,19 @@ function fetchProblems(count,fileName,problemsArray) {
   fs.readFile('./public/problemsList/'+fileName+'.txt',function(err,data){
     data=data.toString();
     data=data.split(',');
-    i=0;
-    while(i!=count)
+    for(i=0;i<count;i++)
     {
-        problemNumber=parseInt((Math.random()*10000)%(data.length-1));
-        problem=data[problemNumber];
-        request({
-          url: 'https://www.codechef.com/api/contests/PRACTICE/problems/'+problem,
-          method: "GET",
-        },function(error,response,body) {
-          e=0;
-          try {
-          problemObject=JSON.parse(body);
-        }catch(e){console.log(e);e=1;i--;}
-        if(e==0){
-          name=problemObject.problem_code;
-          content=problemObject.body;
-          problemsArray.push({'name':name,'content':content});
-        }
-        });
-      i++;
+      problemNumber=parseInt((Math.random()*10000)%(data.length-1));
+      problem=data[problemNumber];
+      request({
+        url: 'https://www.codechef.com/api/contests/PRACTICE/problems/'+problem,
+        method: "GET",
+      },function(error,response,body) {
+        problemObject=JSON.parse(body);
+        name=problemObject.problem_code;
+        content=problemObject.body;
+        problemsArray.push({'name':name,'content':content});
+      });
     }
   });
 }
